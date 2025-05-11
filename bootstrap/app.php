6011dc96bdef12b17c392692d0f88666e46d1a55
nonamich <dimaeromin97@gmail.com>
2025-05-11 23:12:37 +0300

    Add main application layout view with Inertia.js integration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,13 +1,18 @@
 <?php
 
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: __DIR__ . '/../routes/web.php',
     )
-    ->withMiddleware()
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+        ]);
+    })
     ->withExceptions()
     ->create();
