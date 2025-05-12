0fcbe595254d68baf9e9eb814ac3f92577e41e89
nonamich <dimaeromin97@gmail.com>
2025-05-12 14:43:40 +0300

    feat: integrate Inertia.js with Vue 3 and Tailwind CSS
    - Added Inertia.js and Vue 3 support in the application.
    - Created a new HomepageController to handle the homepage route.
    - Implemented a Layout component for consistent page structure.
    - Developed a Homepage view with a welcome message.
    - Updated Vite configuration for improved development server settings.
    - Removed unused bootstrap.js file and integrated Axios directly in app.js.
    - Enhanced app.blade.php with Tailwind CSS classes for styling.
    - Updated package.json to include necessary dependencies for Vue and Inertia.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html class="h-full bg-gray-100">
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 		@vite('resources/js/app.js')
 		@inertiaHead
 	</head>
-	<body>
+	<body class="font-sans leading-none text-gray-700 antialiased">
 		@inertia
 	</body>
 </html>
